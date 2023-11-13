e9044b1ed1c3ffe4e9ccc1b2a781b4307807096c
Nurodayo <cianuroicecream@gmail.com>
2023-11-13 15:43:19 -0300

    clean

--- JAVA/control2.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class control2{
-    public static void main(String[] Args){
-        System.out.println("Adis mundo?")
-    }
-}
--- POKEMON/Entrenador.java
+++
@@ -1,8 +0,0 @@
-public class Entrenador{
-    String nombre;
-    Pokemon pokemon;
-    Entrenador(String nombre, Pokemon pokemon){
-        this.nombre = nombre;
-        this.pokemon = Pokemon;
-    }
-}
